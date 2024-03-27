7159ece4c1ea5239cdbd9bc79c0b53f602d8f293
sJagstang <132593571+sJagstang@users.noreply.github.com>
2024-03-27 13:31:19 +0000

    Implemented credits section on settings page

--- src/Components/SettingsPage.tsx
+++ src/Components/SettingsPage.tsx
@@ -1,6 +1,9 @@
 import '../Components/App.tsx'
 import { ipcRenderer } from 'electron';
 import NavBar from './NavBar.tsx';
+import igdbLogo from '../assets/igdb_transparent.png'
+import icons8logo from '../assets/icons8-logo-4D126FBA84-seeklogo.com.png'
+import flatIconLogo from '../assets/Flaticon.png'
 import '../Styles/SettingsPage.css'
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -83,72 +86,108 @@ function SettingsPage() {
         handleSignOut()
     }
 
+    function openLink(productLink: any) {
+        ipcRenderer.send('openLink', productLink)
+    }
+
+
     return (
         <>
             <div>
                 <NavBar />
             </div>
 
-            <div className='signOut'>
-                <h3 className='settingsHeader'>Sign out of account</h3>
+            <div className='settingsContainer'>
+                <h2 className='topHeader'>Settings</h2>
 
-                <div>
-                    <p className='settingsText'>Click here to sign out of your account</p>
+                <div className='signOut'>
+                    <h3 className='settingsHeader'>Sign out of account</h3>
 
-                    <button className='settingsButton' onClick={handleSignOut}>Sign out</button>
-                </div>
-            </div>
+                    <div>
+                        <p className='settingsText'>Click here to sign out of your account</p>
 
-            <div className='refundProduct'>
-                <h3 className='settingsHeader'>Refund Product</h3>
-
-                <div>
-                    <p className='refundProductText'>Choose a product to refund from your purchased products</p>
-                    <select className='refundProductDropdown' value={selectedProduct} onChange={(e) => setSelectedProduct(e.target.value)}>
-                        <option value="">-- Select a Product to Refund --</option>
-                        {productData.map(product => (
-                            <option key={product.ProductID} value={product.ProductID}>
-                                {product.ProductName} -- £{product.CostOfPurchase}
-                            </option>
-                        ))}
-                    </select>
-
-                    <p className='refundProductText'>Click here to refund the product</p>
-
-                    <button className='refundProductButton' onClick={(handleRefund)}>Refund Product</button>
-                </div>
-            </div>
-
-            <div className='deleteAccount'>
-                <h3 className='settingsHeader'>Delete account</h3>
-
-                <div>
-                    <p className='deleteAccountText'>This will remove all account information and product purchases permanently</p>
-                    <p className='deleteAccountText'>Click here to delete account</p>
-
-                    <button className='deleteAccountButton' onClick={(confirmDelete)}>Delete Account</button>
-                </div>
-            </div>
-
-            {showModal && (
-                <div className='modalOverlay'>
-                    <div className='modalContent'>
-                        <p className='modalText'>Are you sure you want to delete your account?<br />This is non-reversible!</p>
-                        <button className='modalButton' onClick={handleConfirm}>Yes</button>
-                        <button className='modalButton' onClick={handleCancel}>No</button>
+                        <button className='settingsButton' onClick={handleSignOut}>Sign out</button>
                     </div>
                 </div>
-            )}
 
-            {/* Modal for showing refund status */}
-            {refundModalMessage && showRefundModal && (
-                <div className="modalOverlay">
-                    <div className="modalContent">
-                        <p>{refundModalMessage}</p>
-                        <button className="refundModalButton" onClick={handleRefundModal}>Close</button>
+                <div className='refundProduct'>
+                    <h3 className='settingsHeader'>Refund Product</h3>
+
+                    <div>
+                        <p className='refundProductText'>Choose a product to refund and remove from your purchased products</p>
+                        <select className='refundProductDropdown' value={selectedProduct} onChange={(e) => setSelectedProduct(e.target.value)}>
+                            <option value="">-- Select a Product to Refund --</option>
+                            {productData.map(product => (
+                                <option key={product.ProductID} value={product.ProductID}>
+                                    {product.ProductName} -- £{product.CostOfPurchase}
+                                </option>
+                            ))}
+                        </select>
+
+                        <button className='refundProductButton' onClick={(handleRefund)}>Refund Product</button>
                     </div>
                 </div>
-            )}
+
+                <div className='deleteAccount'>
+                    <h3 className='settingsHeader'>Delete account</h3>
+
+                    <div>
+                        <p className='deleteAccountText'>This will remove all account information and product purchases permanently</p>
+                        <p className='deleteAccountText'>Click here to delete your account permanently.</p>
+
+                        <button className='deleteAccountButton' onClick={(confirmDelete)}>Delete Account</button>
+                    </div>
+                </div>
+
+                <div className='appCredits'>
+                    <h3 className='settingsHeader'>Credits</h3>
+                    <div className='creditsContent'>
+                        <p className='creditsText'>Product data provided by: </p>
+                        <button onClick={() => openLink("https://www.igdb.com")} className='igdb-logo'>
+                            <img src={igdbLogo} className='igdb-logo' />
+                        </button>
+                    </div>
+
+                    <div className='creditsContent'>
+                        <p className='creditsText'>Search Magnifying Glass Icon Provided By: </p>
+                        <button onClick={() => openLink("https://icons8.com/icons")} className='icons8-logo'>
+                            <img src={icons8logo} className='icons8-logo' />
+                        </button>
+                    </div>
+
+                    <div className='creditsContent'>
+                        <p className='creditsText'>Controller Logo Provided By:  </p>
+                        <button onClick={() => openLink("https://www.flaticon.com/")} className='flatIcon-logo'>
+                            <img src={flatIconLogo} className='flatIcon-logo' />
+                        </button>
+                    </div>
+
+                    <div className='creditsContent'>
+                        <p className='creditsText'>All products retrieved from IGDB are the property of their respective copyright holders.</p>
+                    </div>
+                </div>
+
+                {/* Modal for showing delete account warning */}
+                {showModal && (
+                    <div className='modalOverlay'>
+                        <div className='modalContent'>
+                            <p className='modalText'>Are you sure you want to delete your account?<br />This is non-reversible!</p>
+                            <button className='modalButton' onClick={handleConfirm}>Yes</button>
+                            <button className='modalButton' onClick={handleCancel}>No</button>
+                        </div>
+                    </div>
+                )}
+
+                {/* Modal for showing refund status */}
+                {refundModalMessage && showRefundModal && (
+                    <div className="modalOverlay">
+                        <div className="modalContent">
+                            <p>{refundModalMessage}</p>
+                            <button className="refundModalButton" onClick={handleRefundModal}>Close</button>
+                        </div>
+                    </div>
+                )}
+            </div>
         </>
     )
 }
